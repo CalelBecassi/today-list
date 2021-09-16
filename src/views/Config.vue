65e51611b670a3585f180aa708bf78f63fa88941
Samuel Willian <sw23022001@gmail.com>
2021-09-16 00:13:23 -0400

    adicionado vue router ao projeto

---
+++ src/views/Config.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>TELA DE CONFIGURAÇÃO OU O COMPONENTE BARRA LATERAL</h1>
+</template>
+
+<script>
+export default {
+    name: 'Config',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>AQUI VAI A TELA DE LOGIN !!</h1>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

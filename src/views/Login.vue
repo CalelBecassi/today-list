39e5febf139d9bcbcbf4b6e61e6661299b639387
thaislarener <thaisla_renner@hotmail.com>
2021-09-17 19:18:10 -0300

    alterando style do v-sheet

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,64 +1,68 @@
 <template>
   <div class="vh-100 vw-100 row-login">
     <v-container>
-      <v-row>
-        <v-col
-          sm="4"
-          class="d-flex justify-content-center align-items-center left-login"
-        >
+      <v-sheet
+        class="d-flex mx-auto mt-16"
+        color="white"
+        elevation="16"
+        height="600"
+        outlined
+        rounded
+        shaped
+        width="800"
+      >
+        <v-col sm="4" class="d-flex left-login">
+          <v-img
+            alt="ToDay Login"
+            class="shrink mr-2"
+            contain
+            src="../assets/logo-login.png"
+            transition="scale-transition"
+            width="180"
+          ></v-img>
         </v-col>
-    
-        <v-col
-          sm="6"
-          class="justify-content-center align-items-center"
-        >
-          <v-form>   
-          <div class="col-8">
-            <div><h2>Login</h2></div>
-            <v-text-field
-              v-model="email"
-              label="E-mail"
-              solo
-              prepend-inner-icon
-            ></v-text-field>
-                
-            <v-text-field
-              v-model="senha"
-              label="Senha"
-              solo
-              prepend-inner-icon
-              
-            ></v-text-field>
+
+        <v-col sm="6" class>
+          <div>
+            <v-form class="mt-14">
+              <div class="text-center"><h2>Login</h2></div>
+              <v-text-field
+                v-model="email"
+                label="E-mail"
+                solo
+                prepend-inner-icon
+              ></v-text-field>
+
+              <v-text-field
+                v-model="senha"
+                label="Senha"
+                solo
+                prepend-inner-icon
+              ></v-text-field>
+              <v-btn elevation="2" color="#f2aa6b" dark large>Entrar</v-btn>
+            </v-form>
           </div>
-          </v-form> 
-        
-          <v-btn
-            elevation="2"
-            color="#f2aa6b"
-            dark
-            large
-          >Entrar</v-btn>
         </v-col>
-      </v-row>
+      </v-sheet>
     </v-container>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Login',
-    data(){
-      return{};
-    },
-  }
+export default {
+  name: "Login",
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .left-login{
-    background-color: #f2aa6b;
-  }
+.left-login {
+  background-color: #f2aa6b;
+}
 
-  .row-login{
-    margin: 0;
-  }
+.row-login {
+  margin: 0;
+}
 </style>

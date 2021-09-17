34aa4fa4f39d99deaac5ddc3bba9c496056ecb51
thaislarener <thaisla_renner@hotmail.com>
2021-09-17 00:51:44 -0300

    corrigi identação

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,61 +1,64 @@
 <template>
-    <div class="row-login">
-        <v-container>
-        <v-row>
-            <v-col
-            cols=5
-            sm=3
-            class="justify-content-center align-items-center left-login"
-            >
-            </v-col>
+  <div class="vh-100 vw-100 row-login">
+    <v-container>
+      <v-row>
+        <v-col
+          sm="4"
+          class="d-flex justify-content-center align-items-center left-login"
+        >
+        </v-col>
     
-            <v-col
-            cols="7"
-            sm="6"
-            class="justify-content-center align-items-center"
-            >
-            <v-form>   
+        <v-col
+          sm="6"
+          class="justify-content-center align-items-center"
+        >
+          <v-form>   
+          <div class="col-8">
+            <div><h2>Login</h2></div>
             <v-text-field
-                v-model="first"
-                label="E-mail"
-                solo
+              v-model="email"
+              label="E-mail"
+              solo
+              prepend-inner-icon
             ></v-text-field>
                 
             <v-text-field
-                v-model="last"
-                label="Senha"
-                solo
+              v-model="senha"
+              label="Senha"
+              solo
+              prepend-inner-icon
+              
             ></v-text-field>
-            </v-form> 
-            <v-btn
-                elevation="2"
-                color="#f2aa6b"
-                dark
-                large
-            >Entrar</v-btn>
-            </v-col>
-        </v-row>
-        </v-container>
-    </div>
+          </div>
+          </v-form> 
+        
+          <v-btn
+            elevation="2"
+            color="#f2aa6b"
+            dark
+            large
+          >Entrar</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
-
 <script>
-export default {
+  export default {
     name: 'Login',
     data(){
-        return{};
+      return{};
     },
-}
+  }
 </script>
 
 <style lang="scss" scoped>
-.left-login{
+  .left-login{
     background-color: #f2aa6b;
-}
+  }
 
-.row-login{
-    margin: 15% 30% 15%;
-}
+  .row-login{
+    margin: 0;
+  }
 </style>
-

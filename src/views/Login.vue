168b2b8a443c829e1587f9b727987e0caea5d470
thaislarener <thaisla_renner@hotmail.com>
2021-09-18 15:30:24 -0300

    colocando icons

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="vh-100 vw-100 row-login">
+  <div class="row-login">
     <v-container>
       <v-sheet
         class="d-flex mx-auto mt-16"
@@ -12,34 +12,39 @@
         width="800"
       >
         <v-col sm="4" class="d-flex left-login">
-          <v-img
-            alt="ToDay Login"
-            class="shrink mr-2"
-            contain
-            src="../assets/logo-login.png"
-            transition="scale-transition"
-            width="180"
-          ></v-img>
+          <div class="d-flex mx-auto">
+            <v-img
+              alt="ToDay Login"
+              class="shrink mr-2"
+              contain
+              src="../assets/logo-login.png"
+              transition="scale-transition"
+              width="180"
+            ></v-img>
+          </div>
         </v-col>
 
-        <v-col sm="6" class>
-          <div>
-            <v-form class="mt-14">
+        <v-col sm="8" class>
+          <div class="col-8 mx-auto mt-15">
+            <v-form>
               <div class="text-center"><h2>Login</h2></div>
+              
               <v-text-field
                 v-model="email"
                 label="E-mail"
-                solo
-                prepend-inner-icon
+                prepend-inner-icon="mdi-email"
+                dense
+                solo-inverted
               ></v-text-field>
 
               <v-text-field
                 v-model="senha"
                 label="Senha"
-                solo
-                prepend-inner-icon
+                prepend-inner-icon="mdi-lock"
+                dense
+                solo-inverted
               ></v-text-field>
-              <v-btn elevation="2" color="#f2aa6b" dark large>Entrar</v-btn>
+              <v-btn elevation="5" color="#f2aa6b" dark block large>Entrar</v-btn>
             </v-form>
           </div>
         </v-col>

602e2292152774097bc09931e361123b2f8af5a6
thaislarener <thaisla_renner@hotmail.com>
2021-09-18 15:42:11 -0300

    tela de registro

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,3 +1,81 @@
 <template>
-    <h1>aqui vai a tela de registro</h1>
+  <div class="row-login">
+    <v-container>
+      <v-sheet
+        class="d-flex mx-auto mt-16"
+        color="white"
+        elevation="16"
+        height="600"
+        outlined
+        rounded
+        shaped
+        width="800"
+      >
+        <v-col sm="4" class="d-flex left-login">
+          <div class="d-flex mx-auto">
+            <v-img
+              alt="ToDay Login"
+              class="shrink mr-2"
+              contain
+              src="../assets/logo-login.png"
+              transition="scale-transition"
+              width="180"
+            ></v-img>
+          </div>
+        </v-col>
+
+        <v-col sm="8" class>
+          <div class="col-8 mx-auto mt-15">
+            <v-form>
+              <div class="text-center"><h2>Cadastre-se</h2></div>
+              
+              <v-text-field
+                v-model="email"
+                label="E-mail"
+                prepend-inner-icon="mdi-email"
+                dense
+                solo-inverted
+              ></v-text-field>
+
+              <v-text-field
+                v-model="senha"
+                label="Senha"
+                prepend-inner-icon="mdi-lock"
+                dense
+                solo-inverted
+              ></v-text-field>
+
+              <v-text-field
+                v-model="confirmasenha"
+                label="Confirme a senha"
+                prepend-inner-icon="mdi-lock"
+                dense
+                solo-inverted
+              ></v-text-field>
+              <v-btn elevation="5" color="#f2aa6b" dark block large>Confirmar</v-btn>
+            </v-form>
+          </div>
+        </v-col>
+      </v-sheet>
+    </v-container>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.left-login {
+  background-color: #f2aa6b;
+}
+
+.row-login {
+  margin: 0;
+}
+</style>
